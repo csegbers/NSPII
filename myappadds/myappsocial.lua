cff35b9f9eb96a6313a26613106f062d6e9914ac
csegbers <csegbers@gmail.com>
2017-03-07 15:11:34 -0500

    miscer wer er g
    sdfsdfsdfsd s df

--- myappadds/myappsocial.lua
+++ myappadds/myappsocial.lua
@@ -3,25 +3,19 @@
 -- Store variables used across the entire app 
 -------------------------------------------------------
 local social = { 
-
---------------------------------------------------------
--- note: physical order of items does not matter. 
--- Order is based on alphabetical based on key 
---------------------------------------------------------
-
-            groupwidth = 120,
+           groupwidth = 120,
             groupmaxwidth = 170,     -- we will allow to grow to fit better if there is extra edging. This would be max however
             groupheight = 140,
             groupheaderheight = 30,
             groupbetween = 10,
-            --groupbackground = { r=255/255, g=255/255, b=255/255, a=1 },
+            groupbackground = { r=255/255, g=255/255, b=255/255, a=1 },
             groupheader = { r=90/255, g=50/255, b=90/255, a=1 },
             groupbackgroundstyle = {
                                       type = 'gradient',
-                                      color1 = { 250/255, 225/255, 250/255, 1 }, 
+                                      color1 = { 225/255, 245/255, 235/255, 1 }, 
                                       color2 = { 250/255, 250/255, 250/255, 1 },
                                       direction = "down"
-                               },
+                               }, 
             iconwidth = 60,    -- can be overidden in item
             iconheight = 60,   -- can be overidden in item
             headercolor = { r=255/255, g=255/255, b=255/255, a=1 },   
@@ -36,6 +30,12 @@ local social = {
                               text="Search NSPII On LinkedIn",
                               doublewide=true,
                               backtext = "<",
+                                groupheaderstyle = {
+                                      type = 'gradient',
+                                      color1 = { 43/255, 143/255, 82/255, 1 }, 
+                                      color2 = { 161/255, 200/255, 177/255, 1 },
+                                      direction = "down"
+                                      },
                              navigation = { app = {
                                     --url="twitter://user?screen_name=StateAuto",
                                     urlfallback = "http://www.linkedin.com/groups?gid=3733613&sik=1364930442625&viewmember",
@@ -48,6 +48,12 @@ local social = {
                               text="Search NSPII On Twitter",
                               doublewide=true,
                               backtext = "<",
+                                groupheaderstyle = {
+                                      type = 'gradient',
+                                      color1 = { 43/255, 143/255, 82/255, 1 }, 
+                                      color2 = { 161/255, 200/255, 177/255, 1 },
+                                      direction = "down"
+                                      },
                              navigation = { app = {
                                     --url="twitter://user?screen_name=StateAuto",
                                     urlfallback = "https://twitter.com/search?q=nspii",
